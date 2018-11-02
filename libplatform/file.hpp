906585d7a3fba3fc5abb76a10773156d18066ded
Matt Tyson <mtyson@mtyson.id.au>
2018-11-02 12:38:02 +1000

    Improve the comments on the file class.

--- libplatform/file.hpp
+++ libplatform/file.hpp
@@ -11,8 +11,8 @@ class file {
 public:
 
 	enum class seek_dir {
-		beg, // seek from beginning
-		end, // seek from end
+		beg, // seek from beginning of file
+		end, // seek from end of file
 		cur  // seek from current location
 	};
 
@@ -23,10 +23,10 @@ public:
 	};
 
 	enum class create_mode {
-		open,   // if exist, open. if not, create.
-		exist,  // if exists, open, if not, error.
-		create, // if exists, error. if not, create.
-		trunc,  // if exists, truncate.  if not, create.
+		open,   // if exists, open.     if not, create.
+		exist,  // if exists, open.     if not, error.
+		create, // if exists, error.    if not, create.
+		trunc,  // if exists, truncate. if not, create.
 	};
 
 public:
@@ -43,18 +43,20 @@ public:
 
 	bool open(const platform::char_t *filename, io_mode iomode, create_mode cmode);
 	bool open(const platform::string_t &filename, io_mode iomode, create_mode cmode);
+
+	// Close the file.  Will be done automatically on object destruction.
 	void close();
 
-	// True if the file is open
+	// True if the file is open, False if not.
 	bool is_open();
 
-	// Set the file to empty with a length of zero bytes
+	// Set the file to empty with a length of zero bytes.
 	void truncate();
 
-	// Set the current file pointer location to be the end of the file
+	// Set the current file pointer location to be the end of the file.
 	void trim();
 
-	// Flush the buffers to disk
+	// Call the underlying operating system flush routine.
 	void flush();
 
 	// Current position in file.
@@ -63,22 +65,29 @@ public:
 	// File length.
 	size_t length();
 
-	// See within the file.
+	// See within the file.  Returns new file pointer position.
 	size_t seek(int64_t offset, seek_dir seek_direction);
 
-	// Writes text out as UTF8 data.
+	// Writes text out as UTF8 data. Returns the number of bytes written.
+	// Note that this function converts 'data' to UTF8 if it is not already.
+	// The number of bytes written to disk may not be the same as 'data';
 	size_t write_utf8(const platform::string_t &data);
 	size_t write_utf8(const platform::char_t *data, size_t length);
 
 	// Read text in as UTF8 and convert it to platform UTF if needed.
 	// Note, this will slurp the whole file into memory.
 	// Slurps from the current location to the end of the file.
+
+	// Returns the number of bytes read from disk.  This may not be the
+	// same as the number of characters in 'data'.
 	size_t read_utf8(platform::string_t &data);
 
-	// Write some bytes of data out to disk.
+	// Write 'length' bytes from 'data' out to disk.
+	// Returns the number of bytes written.
 	size_t write(const char *data, size_t length);
 
-	// Read some random bits of data from disk
+	// Read read 'length' bytes from disk into 'data'
+	// Returns the number of bytes read.
 	size_t read(char *data, size_t length);
 
 private:
